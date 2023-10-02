aa21bacf702ccb3ceaebdedec116ba0994683f63
sido69 <yoann.croguennec@gmail.com>
2023-10-03 01:43:19 +0200

    first commit

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   realisators: { type: String },
   actors: { type: String },
   desc: { type: String },
-  trailer: { type: String },
+  trailer: { type: String, unique: true },
   favorite: { type: Boolean, default: false },
   watch: { type: Boolean, default: false },
   country: { type: String },
   productionCompany: { type: String },
-  movieLink: { type: String },
+  movieLink: { type: String, unique: true },
   img: { type: String, required: true },
   year: { type: Number },
   genre: { type: Array },
